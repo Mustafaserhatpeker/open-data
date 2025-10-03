c36bc3160e84e93bb3ef98dd47be5018b5df8dd2
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-10-04 01:51:51 +0300

    feat: add CardSwap component for animated card swapping functionality

--- opendata/src/pages/Home/home-desktop/HomeDesktop.tsx
+++ opendata/src/pages/Home/home-desktop/HomeDesktop.tsx
@@ -1,14 +1,40 @@
 import Welcome from "./components/Welcome";
 import IntroductionArea from "./components/IntroductionArea";
+import CardSwap, { Card } from '@/components/CardSwap';
 
 function HomeDesktop() {
-
-
   return (
-    <div className=" flex flex-col items-center justify-start  ">
+    <div className="flex flex-col items-center justify-start bg-[#6B61F1]">
       <Welcome />
-
       <IntroductionArea />
+
+      <div style={{ height: '600px', position: 'relative', width: '100%' }} className="flex place-items-start mb-32">
+        <CardSwap
+          cardDistance={60}
+          verticalDistance={70}
+          delay={5000}
+          pauseOnHover={false}
+        >
+          <Card>
+            <div className="p-6">
+              <h3 className="text-xl font-semibold">Card 1</h3>
+              <p>Your content here</p>
+            </div>
+          </Card>
+          <Card>
+            <div className="p-6">
+              <h3 className="text-xl font-semibold">Card 2</h3>
+              <p>Your content here</p>
+            </div>
+          </Card>
+          <Card>
+            <div className="p-6">
+              <h3 className="text-xl font-semibold">Card 3</h3>
+              <p>Your content here</p>
+            </div>
+          </Card>
+        </CardSwap>
+      </div>
     </div>
   );
 }
