6884378d2ce60369cfd386ac61d343e80df612c2
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-10-04 02:11:03 +0300

    Refactor code structure for improved readability and maintainability

--- opendata/src/pages/Home/home-desktop/HomeDesktop.tsx
+++ opendata/src/pages/Home/home-desktop/HomeDesktop.tsx
@@ -1,7 +1,9 @@
 import Welcome from "./components/Welcome";
 import IntroductionArea from "./components/IntroductionArea";
 import CardSwap, { Card } from '@/components/CardSwap';
-
+import photo1 from '@/assets/photo1.png';
+import photo2 from '@/assets/photo2.png';
+import photo3 from '@/assets/photo3.png';
 function HomeDesktop() {
   return (
     <div className="flex flex-col items-center justify-start bg-[#6B61F1]">
@@ -20,20 +22,21 @@ function HomeDesktop() {
         >
           <Card>
             <div className="p-6">
-              <h3 className="text-xl font-semibold">Card 1</h3>
-              <p>Your content here</p>
+              <h3 className="text-xl font-semibold">Veri Setlerinde Gezinin</h3>
+              <img className="h-90" src={photo1} alt="" />
             </div>
           </Card>
           <Card>
             <div className="p-6">
-              <h3 className="text-xl font-semibold">Card 2</h3>
-              <p>Your content here</p>
+              <h3 className="text-xl font-semibold">Organizasyonlara Göz Atın</h3>
+              <img className="h-90" src={photo2} alt="" />
+
             </div>
           </Card>
           <Card>
             <div className="p-6">
-              <h3 className="text-xl font-semibold">Card 3</h3>
-              <p>Your content here</p>
+              <h3 className="text-xl font-semibold">Bulamadığınız Veri Setini Talep Edin</h3>
+              <img className="h-90" src={photo3} alt="" />
             </div>
           </Card>
         </CardSwap>
