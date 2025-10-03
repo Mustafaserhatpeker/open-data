929ef30e8b722edbe2765f3be38c7e2ed80df8ff
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-10-03 23:25:42 +0300

    refactor: update RotatingText import and usage in Welcome component

--- opendata/src/pages/Home/home-desktop/components/Welcome.tsx
+++ opendata/src/pages/Home/home-desktop/components/Welcome.tsx
@@ -1,3 +1,4 @@
+import RotatingText from "@/components/RotatingText";
 function Welcome() {
   return (
     <div
@@ -7,7 +8,18 @@ function Welcome() {
       }}
       className="w-full min-h-[100vh] flex flex-col items-center justify-start py-24"
     >
-      <h1 className="text-4xl font-bold mb-4">Welcome to Our Open Data Platform</h1>
+      <RotatingText
+        texts={['React', 'Bits', 'Is', 'Cool!']}
+        mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+        staggerFrom={"last"}
+        initial={{ y: "100%" }}
+        animate={{ y: 0 }}
+        exit={{ y: "-120%" }}
+        staggerDuration={0.025}
+        splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+        transition={{ type: "spring", damping: 30, stiffness: 400 }}
+        rotationInterval={2000}
+      />
     </div>
   );
 }
