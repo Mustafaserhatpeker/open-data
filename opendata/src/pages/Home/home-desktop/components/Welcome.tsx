ac8320a616c12234f8e9e69a3d95bf046886f707
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-10-04 20:50:16 +0300

    feat: update tooltip content and icons for improved data representation in Welcome component

--- opendata/src/pages/Home/home-desktop/components/Welcome.tsx
+++ opendata/src/pages/Home/home-desktop/components/Welcome.tsx
@@ -1,83 +1,103 @@
 import RotatingText from "@/components/RotatingText";
 import { Button } from "@/components/ui/button";
 import Background from "@/assets/background.svg";
-import { Droplets, MousePointerClick } from "lucide-react";
+import { Building2, Bus, CarFront, Droplets, MousePointerClick, Ship, Trees, WavesLadder } from "lucide-react";
 import { HomeTooltip } from "./inner-components/HomeTooltip";
 function Welcome() {
   const tooltips = [
     {
-      id: 1, content: <div className="flex flex-col items-start justify-between">
+      id: 1,
+      content: <div className="flex flex-col items-start justify-between">
         <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
           <Droplets />
-          Deniz Verileri</h1>
+          Deniz Verileri
+        </h1>
         <p className="text-start font-bold text-lg">
-          Bu veri seti, Kocaeli Büyükşehir Belediyesi'nin çevre ile ilgili çeşitli ölçümlerini ve analizlerini içermektedir. Hava kalitesi, su kalitesi, atık yönetimi ve diğer çevresel faktörler hakkında detaylı bilgiler sunar.
+          Kocaeli’nin deniz ve kıyı bölgelerine ait ölçümler sunar. Su kalitesi, sıcaklık, tuzluluk ve deniz canlılığına dair bilgiler içerir. Deniz kirliliği ve ekosistem sağlığına ilişkin analizler yapılabilir.
         </p>
       </div>,
       clasName: "absolute top-10 left-72 w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
-
     },
     {
-      id: 2, content: <div className="flex flex-col items-start justify-between">
-        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">Trafik Verisi</h1>
+      id: 2,
+      content: <div className="flex flex-col items-start justify-between">
+        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+          <CarFront />
+          Trafik Verisi
+        </h1>
         <p className="text-start font-bold text-lg">
-          Bu veri seti, Kocaeli Büyükşehir Belediyesi'nin çevre ile ilgili çeşitli ölçümlerini ve analizlerini içermektedir. Hava kalitesi, su kalitesi, atık yönetimi ve diğer çevresel faktörler hakkında detaylı bilgiler sunar.
+          Kocaeli genelinde trafik yoğunluğu, araç sayıları, kavşak geçiş süreleri ve yol kullanım istatistiklerini içerir. Trafik planlaması ve ulaşım stratejileri için kullanılabilir.
         </p>
       </div>,
       clasName: "absolute top-58 left-2/7  w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
-
     },
     {
-      id: 3, content: <div className="flex flex-col items-start justify-between">
-        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">Çevre Verisi</h1>
+      id: 3,
+      content: <div className="flex flex-col items-start justify-between">
+        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+          <Bus />
+          Toplu Taşıma Verisi
+        </h1>
         <p className="text-start font-bold text-lg">
-          Bu veri seti, Kocaeli Büyükşehir Belediyesi'nin çevre ile ilgili çeşitli ölçümlerini ve analizlerini içermektedir. Hava kalitesi, su kalitesi, atık yönetimi ve diğer çevresel faktörler hakkında detaylı bilgiler sunar.
+          Otobüs, tramvay ve diğer toplu taşıma araçlarına dair sefer saatleri, hat yoğunlukları, yolcu sayıları ve durak bazlı istatistikler yer alır. Şehir içi ulaşım planlaması için kullanılabilir.
         </p>
       </div>,
       clasName: "absolute top-50 right-1/3  w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
-
     },
     {
-      id: 4, content: <div className="flex flex-col items-start justify-between">
-        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">Çevre Verisi</h1>
+      id: 4,
+      content: <div className="flex flex-col items-start justify-between">
+        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+          <Trees />
+          Çevre ve Orman Verisi
+        </h1>
         <p className="text-start font-bold text-lg">
-          Bu veri seti, Kocaeli Büyükşehir Belediyesi'nin çevre ile ilgili çeşitli ölçümlerini ve analizlerini içermektedir. Hava kalitesi, su kalitesi, atık yönetimi ve diğer çevresel faktörler hakkında detaylı bilgiler sunar.
+          Orman alanları, yeşil alanlar, ağaçlandırma çalışmaları ve biyolojik çeşitlilik verilerini kapsar. Çevre koruma faaliyetleri ve sürdürülebilirlik politikaları için kaynak sağlar.
         </p>
       </div>,
       clasName: "absolute top-92 left-2/5  w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
-
     },
     {
-      id: 5, content: <div className="flex flex-col items-start justify-between">
-        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">Çevre Verisi</h1>
+      id: 5,
+      content: <div className="flex flex-col items-start justify-between">
+        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+          <WavesLadder />
+          Göl ve Kaynak Su Verisi
+        </h1>
         <p className="text-start font-bold text-lg">
-          Bu veri seti, Kocaeli Büyükşehir Belediyesi'nin çevre ile ilgili çeşitli ölçümlerini ve analizlerini içermektedir. Hava kalitesi, su kalitesi, atık yönetimi ve diğer çevresel faktörler hakkında detaylı bilgiler sunar.
+          İçme suyu kaynakları, göl seviyeleri, su kalitesi ve yeraltı suları hakkında ölçümler içerir. Su yönetimi ve çevresel sürdürülebilirlik için önemli göstergeler sağlar.
         </p>
       </div>,
       clasName: "absolute bottom-12 left-1/2  w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
-
     },
     {
-      id: 6, content: <div className="flex flex-col items-start justify-between">
-        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">Çevre Verisi</h1>
+      id: 6,
+      content: <div className="flex flex-col items-start justify-between">
+        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+          <Building2 />
+          Konut Yerleşim Verisi
+        </h1>
         <p className="text-start font-bold text-lg">
-          Bu veri seti, Kocaeli Büyükşehir Belediyesi'nin çevre ile ilgili çeşitli ölçümlerini ve analizlerini içermektedir. Hava kalitesi, su kalitesi, atık yönetimi ve diğer çevresel faktörler hakkında detaylı bilgiler sunar.
+          Kocaeli’deki konut bölgeleri, nüfus yoğunluğu, imar planları ve yapılaşma istatistiklerini içerir. Şehir planlaması ve kentsel gelişim için kullanılabilir.
         </p>
       </div>,
       clasName: "absolute bottom-64 right-1/3  w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
-
     },
     {
-      id: 7, content: <div className="flex flex-col items-start justify-between">
-        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">Çevre Verisi</h1>
+      id: 7,
+      content: <div className="flex flex-col items-start justify-between">
+        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+          <Ship />
+          Liman ve Kıyı Verisi
+        </h1>
         <p className="text-start font-bold text-lg">
-          Bu veri seti, Kocaeli Büyükşehir Belediyesi'nin çevre ile ilgili çeşitli ölçümlerini ve analizlerini içermektedir. Hava kalitesi, su kalitesi, atık yönetimi ve diğer çevresel faktörler hakkında detaylı bilgiler sunar.
+          Kocaeli’deki liman faaliyetleri, yük hareketleri, gemi trafiği ve kıyı yapılarıyla ilgili verileri içerir. Deniz taşımacılığı ve lojistik analizleri için kullanılabilir.
         </p>
       </div>,
       clasName: "absolute bottom-40 right-1/5  w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
-
     },
   ];
+
   return (
     <div
       style={{
