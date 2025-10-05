d8b50129e11ab4fbad5244c9e38bbe3f47c87693
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-10-05 13:30:11 +0300

    feat: enhance DatarequestsDesktop with RightFilter component and search functionality

--- opendata/src/pages/Datarequests/datarequests-desktop/DatarequestsDesktop.tsx
+++ opendata/src/pages/Datarequests/datarequests-desktop/DatarequestsDesktop.tsx
@@ -1,7 +1,97 @@
+import RightFilter from "./components/RightFilter"
+import {
+    SearchIcon,
+} from "lucide-react"
+import {
+    InputGroup,
+    InputGroupAddon,
+    InputGroupButton,
+    InputGroupInput,
+} from "@/components/ui/input-group"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import DatarequestCards from "./components/DatarequestCards"
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from "@/components/ui/pagination"
 
 function DatarequestsDesktop() {
     return (
-        <div>DatarequestsDesktop</div>
+        <div className="w-full flex flex-col items-center justify-between bg-accent  ">
+            <div className="grid grid-cols-4 w-full gap-8 px-4 py-8 max-w-7xl mx-auto ">
+                <div className="col-span-1   rounded-xl">
+                    <RightFilter />
+                </div>
+                <div className="col-span-3 bg-white p-2 rounded-xl">
+                    <div className="grid grid-cols-5 w-full gap-6">
+                        <div className="col-span-4 ">
+                            <InputGroup>
+                                <InputGroupInput placeholder="Type to search..." />
+                                <InputGroupAddon align="inline-end">
+                                    <InputGroupButton variant="secondary">
+                                        <SearchIcon />
+                                    </InputGroupButton>
+                                </InputGroupAddon>
+                            </InputGroup>
+                        </div>
+                        <div className="col-span-1">
+                            <Select defaultValue="newest">
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Sırala" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="newest">En Yeni</SelectItem>
+                                    <SelectItem value="oldest">En Eski</SelectItem>
+                                    <SelectItem value="a-z">A-Z</SelectItem>
+                                    <SelectItem value="z-a">Z-A</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+
+
+                    </div>
+
+                    <div className="mt-4 mb-6">
+                        <DatarequestCards />
+                    </div>
+                    <Pagination>
+                        <PaginationContent>
+                            <PaginationItem>
+                                <PaginationPrevious href="#" />
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationLink href="#">1</PaginationLink>
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationLink href="#" isActive>
+                                    2
+                                </PaginationLink>
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationLink href="#">3</PaginationLink>
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationEllipsis />
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationNext href="#" />
+                            </PaginationItem>
+                        </PaginationContent>
+                    </Pagination>
+                </div>
+            </div>
+        </div>
     )
 }
 
---
+++ opendata/src/pages/Datarequests/datarequests-desktop/components/RightFilter.tsx
@@ -0,0 +1,110 @@
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
+import { Button } from "@/components/ui/button"
+import { PhoneIncomingIcon } from "lucide-react"
+
+export default function RightFilter() {
+    return (
+        <Accordion
+            type="multiple"
+            className="w-full bg-white p-4 rounded-lg "
+        >
+            <AccordionItem value="item-1">
+                <AccordionTrigger>Organizasyonlar</AccordionTrigger>
+                <AccordionContent className="flex flex-col gap-4 text-balance">
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+                <AccordionTrigger>Kategoriler</AccordionTrigger>
+                <AccordionContent className="flex flex-col gap-4 text-balance">
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+                <AccordionTrigger>Etiketler</AccordionTrigger>
+                <AccordionContent className="flex flex-col gap-4 text-balance">
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-4">
+                <AccordionTrigger>Formatlar</AccordionTrigger>
+                <AccordionContent className="flex flex-col gap-4 text-balance">
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <PhoneIncomingIcon /> New Branch
+                    </Button>
+                </AccordionContent>
+            </AccordionItem>
+        </Accordion>
+    )
+}
