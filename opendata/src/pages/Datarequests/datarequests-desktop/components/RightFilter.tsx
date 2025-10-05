3dde0aa3988a2a48a7d5ca90460213d7c0abb1b1
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-10-05 13:49:28 +0300

    feat: refactor RightFilter buttons for improved layout and consistency

--- opendata/src/pages/Datarequests/datarequests-desktop/components/RightFilter.tsx
+++ opendata/src/pages/Datarequests/datarequests-desktop/components/RightFilter.tsx
@@ -4,6 +4,7 @@ import {
     AccordionItem,
     AccordionTrigger,
 } from "@/components/ui/accordion"
+import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { BadgeCheck, BadgeInfo, BadgeX } from "lucide-react"
 
@@ -16,14 +17,21 @@ export default function RightFilter() {
             <AccordionItem value="item-1">
                 <AccordionTrigger>Durum Bilgisi</AccordionTrigger>
                 <AccordionContent className="flex flex-col gap-4 text-balance">
-                    <Button variant="outline" >
-                        <BadgeCheck color="green" /> Onaylandı
+                    <Button className="flex flex-row items-center justify-between" variant="outline" >
+                        <span className="flex flex-row items-center gap-2">
+                            <BadgeCheck color="green" /> Onaylandı
+                        </span>
+                        <Badge variant="outline">17</Badge>
                     </Button>
-                    <Button variant="outline" >
-                        <BadgeInfo color="orange" /> Bekleniyor
+                    <Button className="flex flex-row items-center justify-between" variant="outline" >                        <span className="flex flex-row items-center gap-2">
+                        <BadgeInfo color="blue" /> İnceleniyor
+                    </span>
+                        <Badge variant="outline">5</Badge>
                     </Button>
-                    <Button variant="outline" >
+                    <Button className="flex flex-row items-center justify-between" variant="outline" >                        <span className="flex flex-row items-center gap-2">
                         <BadgeX color="red" /> Reddedildi
+                    </span>
+                        <Badge variant="outline">2</Badge>
                     </Button>
                 </AccordionContent>
             </AccordionItem>
