9a80f28ef117651ad1a568570f8c8fa24241b228
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-10-09 20:11:04 +0300

    fix: remove unnecessary blank line in UserDropdown component

--- opendata/src/components/header/header-desktop/components/UserDropdown.tsx
+++ opendata/src/components/header/header-desktop/components/UserDropdown.tsx
@@ -42,7 +42,6 @@ export default function UserDropdown() {
                     }>
                         Kullanıcı Panelim
                     </DropdownMenuItem>
-
                 </DropdownMenuGroup>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem>
