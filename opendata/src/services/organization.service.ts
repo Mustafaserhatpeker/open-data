6827d56b6f34c0cd9d41786626135ce4e237a729
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-10-12 01:58:05 +0300

    feat: add organization service to fetch all organizations from API

---
+++ opendata/src/services/organization.service.ts
@@ -0,0 +1,12 @@
+import { axiosInstance } from "../lib/axios";
+
+const Base = "/organization";
+
+export const getOrganizations = async () => {
+    try {
+        const { data } = await axiosInstance.get(`${Base}/get-all-organizations`);
+        return data;
+    } catch (error) {
+        throw error;
+    }
+};
