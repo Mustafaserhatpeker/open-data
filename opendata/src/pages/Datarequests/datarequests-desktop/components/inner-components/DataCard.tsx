625f91930c4c6aa56090db327cf682152cbf9bce
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-10-24 04:54:53 +0300

    feat: remove likes count display from DataRequestCard component

--- opendata/src/pages/Datarequests/datarequests-desktop/components/inner-components/DataCard.tsx
+++ opendata/src/pages/Datarequests/datarequests-desktop/components/inner-components/DataCard.tsx
@@ -123,23 +123,7 @@ export default function DataRequestCard({
                     </span>
                 )}
 
-                {request.likesCount !== undefined && (
-                    <span className="inline-flex items-center gap-1 rounded-md bg-gray-50 px-2 py-1">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-3.5 w-3.5"
-                            viewBox="0 0 24 24"
-                            fill="currentColor"
-                        >
-                            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 
-                4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 
-                2.09C13.09 3.81 14.76 3 16.5 
-                3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 
-                6.86-8.55 11.54L12 21.35z" />
-                        </svg>
-                        {request.likesCount}
-                    </span>
-                )}
+
             </div>
 
             {/* Alt kısım: tarih bilgileri */}
