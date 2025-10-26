5eebd7b19907768d02388f19cdb33d76199cd785
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-10-26 19:46:20 +0300

    feat: enhance HomeTooltip component with title and full content props for improved tooltip display

--- opendata/src/pages/Home/home-desktop/components/Welcome.tsx
+++ opendata/src/pages/Home/home-desktop/components/Welcome.tsx
@@ -1,114 +1,199 @@
 import RotatingText from "@/components/RotatingText";
 import { Button } from "@/components/ui/button";
 import Background from "@/assets/background.svg";
-import { Building2, Bus, CarFront, Droplets, MousePointerClick, Ship, Trees, WavesLadder } from "lucide-react";
+import {
+  Building2,
+  Bus,
+  CarFront,
+  Droplets,
+  MousePointerClick,
+  Ship,
+  Trees,
+  WavesLadder
+} from "lucide-react";
 import { HomeTooltip } from "./inner-components/HomeTooltip";
 import BlurText from "@/components/BlurText";
+
 function Welcome() {
   const tooltips = [
     {
       id: 1,
-      content: <div className="flex flex-col items-start justify-between">
-        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+      titleContent: (
+        <>
           <Droplets />
           Deniz Verileri
-        </h1>
-        <p className="text-start font-bold text-lg">
-          Kocaeli’nin deniz ve kıyı bölgelerine ait ölçümler sunar. Su kalitesi, sıcaklık, tuzluluk ve deniz canlılığına dair bilgiler içerir. Deniz kirliliği ve ekosistem sağlığına ilişkin analizler yapılabilir.
-        </p>
-      </div>,
-      clasName: "absolute top-10 left-72 w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
+        </>
+      ),
+      fullContent: (
+        <div className="flex flex-col items-start gap-2">
+          <h1 className="font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+            <Droplets />
+            Deniz Verileri
+          </h1>
+          <p className="text-start font-bold text-lg">
+            Kocaeli’nin deniz ve kıyı bölgelerine ait ölçümler sunar. Su kalitesi,
+            sıcaklık, tuzluluk ve deniz canlılığına dair bilgiler içerir. Deniz
+            kirliliği ve ekosistem sağlığına ilişkin analizler yapılabilir.
+          </p>
+        </div>
+      ),
+      className:
+        "absolute top-10 left-72 w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
     },
     {
       id: 2,
-      content: <div className="flex flex-col items-start justify-between">
-        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+      titleContent: (
+        <>
           <CarFront />
           Trafik Verisi
-        </h1>
-        <p className="text-start font-bold text-lg">
-          Kocaeli genelinde trafik yoğunluğu, araç sayıları, kavşak geçiş süreleri ve yol kullanım istatistiklerini içerir. Trafik planlaması ve ulaşım stratejileri için kullanılabilir.
-        </p>
-      </div>,
-      clasName: "absolute top-58 left-2/7  w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
+        </>
+      ),
+      fullContent: (
+        <div className="flex flex-col items-start gap-2">
+          <h1 className="font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+            <CarFront />
+            Trafik Verisi
+          </h1>
+          <p className="text-start font-bold text-lg">
+            Kocaeli genelinde trafik yoğunluğu, araç sayıları, kavşak geçiş
+            süreleri ve yol kullanım istatistiklerini içerir. Trafik planlaması ve
+            ulaşım stratejileri için kullanılabilir.
+          </p>
+        </div>
+      ),
+      className:
+        "absolute top-58 left-2/7 w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
     },
     {
       id: 3,
-      content: <div className="flex flex-col items-start justify-between">
-        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+      titleContent: (
+        <>
           <Bus />
           Toplu Taşıma Verisi
-        </h1>
-        <p className="text-start font-bold text-lg">
-          Otobüs, tramvay ve diğer toplu taşıma araçlarına dair sefer saatleri, hat yoğunlukları, yolcu sayıları ve durak bazlı istatistikler yer alır. Şehir içi ulaşım planlaması için kullanılabilir.
-        </p>
-      </div>,
-      clasName: "absolute top-50 right-1/3  w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
+        </>
+      ),
+      fullContent: (
+        <div className="flex flex-col items-start gap-2">
+          <h1 className="font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+            <Bus />
+            Toplu Taşıma Verisi
+          </h1>
+          <p className="text-start font-bold text-lg">
+            Otobüs, tramvay ve diğer toplu taşıma araçlarına dair sefer saatleri,
+            hat yoğunlukları, yolcu sayıları ve durak bazlı istatistikler yer
+            alır. Şehir içi ulaşım planlaması için kullanılabilir.
+          </p>
+        </div>
+      ),
+      className:
+        "absolute top-50 right-1/3 w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
     },
     {
       id: 4,
-      content: <div className="flex flex-col items-start justify-between">
-        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+      titleContent: (
+        <>
           <Trees />
-          Çevre ve Orman Verisi
-        </h1>
-        <p className="text-start font-bold text-lg">
-          Orman alanları, yeşil alanlar, ağaçlandırma çalışmaları ve biyolojik çeşitlilik verilerini kapsar. Çevre koruma faaliyetleri ve sürdürülebilirlik politikaları için kaynak sağlar.
-        </p>
-      </div>,
-      clasName: "absolute top-92 left-2/5  w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
+          Çevre & Orman Verisi
+        </>
+      ),
+      fullContent: (
+        <div className="flex flex-col items-start gap-2">
+          <h1 className="font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+            <Trees />
+            Çevre & Orman Verisi
+          </h1>
+          <p className="text-start font-bold text-lg">
+            Orman alanları, yeşil alanlar, ağaçlandırma çalışmaları ve biyolojik
+            çeşitlilik verilerini kapsar. Çevre koruma faaliyetleri ve
+            sürdürülebilirlik politikaları için kaynak sağlar.
+          </p>
+        </div>
+      ),
+      className:
+        "absolute top-92 left-2/5 w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
     },
     {
       id: 5,
-      content: <div className="flex flex-col items-start justify-between">
-        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+      titleContent: (
+        <>
           <WavesLadder />
           Göl ve Kaynak Su Verisi
-        </h1>
-        <p className="text-start font-bold text-lg">
-          İçme suyu kaynakları, göl seviyeleri, su kalitesi ve yeraltı suları hakkında ölçümler içerir. Su yönetimi ve çevresel sürdürülebilirlik için önemli göstergeler sağlar.
-        </p>
-      </div>,
-      clasName: "absolute bottom-12 left-1/2  w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
+        </>
+      ),
+      fullContent: (
+        <div className="flex flex-col items-start gap-2">
+          <h1 className="font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+            <WavesLadder />
+            Göl & Kaynak Su Verisi
+          </h1>
+          <p className="text-start font-bold text-lg">
+            İçme suyu kaynakları, göl seviyeleri, su kalitesi ve yeraltı suları
+            hakkında ölçümler içerir. Su yönetimi ve çevresel sürdürülebilirlik
+            için önemli göstergeler sağlar.
+          </p>
+        </div>
+      ),
+      className:
+        "absolute bottom-12 left-1/2 w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
     },
     {
       id: 6,
-      content: <div className="flex flex-col items-start justify-between">
-        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+      titleContent: (
+        <>
           <Building2 />
-          Konut Yerleşim Verisi
-        </h1>
-        <p className="text-start font-bold text-lg">
-          Kocaeli’deki konut bölgeleri, nüfus yoğunluğu, imar planları ve yapılaşma istatistiklerini içerir. Şehir planlaması ve kentsel gelişim için kullanılabilir.
-        </p>
-      </div>,
-      clasName: "absolute bottom-64 right-1/3  w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
+          Konut Verisi
+        </>
+      ),
+      fullContent: (
+        <div className="flex flex-col items-start gap-2">
+          <h1 className="font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+            <Building2 />
+            Konut Yerleşim Verisi
+          </h1>
+          <p className="text-start font-bold text-lg">
+            Kocaeli’deki konut bölgeleri, nüfus yoğunluğu, imar planları ve
+            yapılaşma istatistiklerini içerir. Şehir planlaması ve kentsel
+            gelişim için kullanılabilir.
+          </p>
+        </div>
+      ),
+      className:
+        "absolute bottom-64 right-1/3 w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
     },
     {
       id: 7,
-      content: <div className="flex flex-col items-start justify-between">
-        <h1 className="text-start font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+      titleContent: (
+        <>
           <Ship />
-          Liman ve Kıyı Verisi
-        </h1>
-        <p className="text-start font-bold text-lg">
-          Kocaeli’deki liman faaliyetleri, yük hareketleri, gemi trafiği ve kıyı yapılarıyla ilgili verileri içerir. Deniz taşımacılığı ve lojistik analizleri için kullanılabilir.
-        </p>
-      </div>,
-      clasName: "absolute bottom-40 right-1/5  w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
-    },
+          Liman & Kıyı Verisi
+        </>
+      ),
+      fullContent: (
+        <div className="flex flex-col items-start gap-2">
+          <h1 className="font-extrabold text-xl flex items-center gap-2 border-b-2 border-white/50 pb-1 w-full">
+            <Ship />
+            Liman & Kıyı Verisi
+          </h1>
+          <p className="text-start font-bold text-lg">
+            Kocaeli’deki liman faaliyetleri, yük hareketleri, gemi trafiği ve kıyı
+            yapılarıyla ilgili verileri içerir. Deniz taşımacılığı ve lojistik
+            analizleri için kullanılabilir.
+          </p>
+        </div>
+      ),
+      className:
+        "absolute bottom-40 right-1/5 w-8 h-8 bg-[#DAFAB2] text-[#7F7DFB] rounded-full"
+    }
   ];
 
   return (
-    <div
-      className="w-full min-h-[100vh] flex flex-col items-center justify-between pt-24"
-    >
-      <div className=" flex flex-col items-center justify-center w-full">
+    <div className="w-full min-h-[100vh] flex flex-col items-center justify-between pt-24">
+      <div className="flex flex-col items-center justify-center w-full">
         <h1 className="text-sm font-extrabold text-center mb-8 flex items-center gap-2 bg-white/70 px-4 py-2 rounded-full">
           Kocaeli Veri Portalında
           <RotatingText
             texts={['Veri İsteyin', 'Veri Bulun', 'Üye Olun', 'Giriş Yapın']}
-            mainClassName=" text-black overflow-hidden w-24 justify-start mt-1"
+            mainClassName="text-black overflow-hidden w-24 justify-start mt-1"
             staggerFrom={"last"}
             initial={{ y: "100%" }}
             animate={{ y: 0 }}
@@ -128,7 +213,10 @@ function Welcome() {
           className="text-6xl mb-8 max-w-7xl text-center font-extrabold"
         />
 
-        <Button variant={"outline"} className="mt-16 px-4 py-6 rounded-md bg-[#6558F6] border-none text-accent">
+        <Button
+          variant={"outline"}
+          className="mt-16 px-4 py-6 rounded-md bg-[#6558F6] border-none text-accent"
+        >
           <a href="datasets" className="text-sm font-semibold">
             Veri Seti Arayın
           </a>
@@ -138,18 +226,17 @@ function Welcome() {
       <div className="relative w-full mt-24">
         <img className="w-[100vw]" src={Background} alt="" />
 
-
         {tooltips.map((tooltip) => (
           <HomeTooltip
             key={tooltip.id}
-            className={tooltip.clasName}
-            content={tooltip.content}
-            children={<MousePointerClick />}
-          />
+            className={tooltip.className}
+            titleContent={tooltip.titleContent}
+            fullContent={tooltip.fullContent}
+          >
+            <MousePointerClick />
+          </HomeTooltip>
         ))}
       </div>
-
-
     </div>
   );
 }
--- opendata/src/pages/Home/home-desktop/components/inner-components/HomeTooltip.tsx
+++ opendata/src/pages/Home/home-desktop/components/inner-components/HomeTooltip.tsx
@@ -4,24 +4,46 @@ import {
     TooltipContent,
     TooltipTrigger,
 } from "@/components/ui/tooltip"
-import type { ReactNode } from "react";
+import { useState, type ReactNode } from "react";
 
 type HomeTooltipProps = {
     className?: string;
-    content?: ReactNode;
+    titleContent: ReactNode;
+    fullContent: ReactNode;
     children?: ReactNode;
 };
-export function HomeTooltip({ className, content, children }: HomeTooltipProps
-) {
+
+export function HomeTooltip({
+    className,
+    titleContent,
+    fullContent,
+    children
+}: HomeTooltipProps) {
+    const [hover, setHover] = useState(false);
+
     return (
         <Tooltip>
             <TooltipTrigger asChild>
-                <Button className={className} variant="outline">
+                <Button
+                    className={`${className} cursor-pointer`}
+                    variant="outline"
+                    onMouseEnter={() => setHover(true)}
+                    onMouseLeave={() => setHover(false)}
+                >
                     {children}
+
+                    <div className="absolute -top-10 left-1/2 -translate-x-1/2 text-white font-bold text-[10px] whitespace-nowrap bg-black/20 pr-8 px-2 py-1 rounded-md">
+                        {titleContent}
+                    </div>
                 </Button>
             </TooltipTrigger>
-            <TooltipContent className="max-w-xs backdrop-blur-2xl bg-black/30 border border-white/30 shadow-lg">
-                {content}
+
+            <TooltipContent
+                className={`max-w-xs backdrop-blur-2xl bg-black/40 border border-white/30 shadow-lg transition-all duration-200
+                ${hover ? "opacity-100 scale-100" : "opacity-0 scale-95 pointer-events-none"}
+                `}
+            >
+                {fullContent}
             </TooltipContent>
         </Tooltip>
     )
