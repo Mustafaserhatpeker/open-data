6024fe509b477454a059e5bf40d16625b73fb8ff
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-10-26 20:16:04 +0300

    fix: adjust grid column count in LabelGrid component for better layout

--- opendata/src/pages/Home/home-desktop/components/LabelGrid.tsx
+++ opendata/src/pages/Home/home-desktop/components/LabelGrid.tsx
@@ -15,7 +15,7 @@ function LabelGrid() {
     return (
         <div className='w-full flex flex-col items-center justify-center bg-[#6A60F2] text-white pt-80 py-8 pb-40  '>
             <span className="text-6xl font-bold max-w-[80%] text-center">Veri Setlerini Kategorilerine Göre Keşfedin</span>
-            <div className='grid grid-cols-5 gap-12 mt-12 '>
+            <div className='grid grid-cols-4 gap-12 mt-12 '>
                 {categoriesResp?.data?.map((category: Category) => (
                     <LabelCard key={category._id} category={category} />
                 ))}
