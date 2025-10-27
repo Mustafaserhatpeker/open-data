d7874efb6a48de9f20176a0685601946ee584a43
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-10-27 11:13:49 +0300

    feat: update API_BASE_URL to point to the correct server address

--- opendata/src/lib/urls.ts
+++ opendata/src/lib/urls.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:3000/api/file"
+const API_BASE_URL = "http://10.100.9.188:3000/api/file"
 export const DOWNLOAD_URL = `${API_BASE_URL}/download`
 export const PREVIEW_URL = `${API_BASE_URL}/inline`
 
