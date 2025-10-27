d5b35be26295779b57e5224b13b59760b4efa96d
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-10-27 05:03:04 +0300

    feat: update axios instance baseURL to use a constant for improved maintainability

--- opendata/src/lib/axios.ts
+++ opendata/src/lib/axios.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import qs from "qs";
+const BaseURL = "http://10.100.9.188:3000/api";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: BaseURL,
   paramsSerializer: (params) =>
     qs.stringify(params, { arrayFormat: "repeat" }),
   withCredentials: true,
